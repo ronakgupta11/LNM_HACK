4ac0b926816df7b8a9bac5d3f092adbbed56a963
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-01-29 09:01:39 +0530

    Update Navbar.js

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar(props){
             </h2> */}
             <div className="avatar">
 
-            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+            <svg  style = {{margin:"10px"}}xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="#f0536c" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
 
             <button className="nav-btn" onClick={props.walletConnected?props.disconnect:props.connectWallet}>
             {props.walletConnected?add:"Connect Wallet"}
